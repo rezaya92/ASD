30ae15d3e4b6884c0beb3a39a12ac9c90c8ce6d8
kmaroufi <kmaroufi76@gmail.com>
2022-02-14 19:23:52 +0330

    Create Content, ContentFeature and Attachment Models

--- libcloud/models.py
+++ libcloud/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+def get_sentinel_user():
+    return User.objects.get_or_create(username='deleted')[0]
+
+
+class Content(Model):
+    class ContentType(models.IntegerChoices):
+        Video = 1
+        Image = 2
+        Book = 3
+        Audio = 4
+        Other = 5
+
+    creator = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    type = models.IntegerField(choices=ContentType.choices)
+    file = models.FileField()
+
+
+class ContentFeature(Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    value = models.CharField(max_length=100)
+
+
+class Attachment(Model):
+    class AttachmentType(models.IntegerChoices):
+        Subtitle = 1
+        Other = 2
+
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    type = models.IntegerField(choices=AttachmentType.choices)
+    file = models.FileField()
