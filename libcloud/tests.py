cf1c360868316d598d633d0ae4bf1023c46edffa
kmaroufi <kmaroufi76@gmail.com>
2022-02-14 22:25:15 +0330

    adding testcases for ContentFeature model

--- libcloud/tests.py
+++ libcloud/tests.py
@@ -38,3 +38,40 @@ class ContentModelTest(TestCase):
     def test_content_create_with_null_file(self):
         with self.assertRaises(ValidationError):
             Content.objects.create(creator=self.user, type=Content.ContentType.Book)
+
+
+class ContentFeatureModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(email='testemail@gmail.com', username='username', password='123')
+        with open('temp.txt', 'w') as f:
+            f.write("Test File")
+
+        with open('temp.txt', 'r') as f:
+            self.content = Content.objects.create(creator=self.user, type=Content.ContentType.Book,
+                                                  file=File(f, 'temp.txt'))
+        return super().setUp()
+
+    def tearDown(self):
+        os.remove("temp.txt")
+
+    def test_correct_content_feature_create(self):
+        ContentFeature.objects.create(content=self.content, name="abc", value="x")
+
+    def test_content_feature_create_with_null_content(self):
+        with self.assertRaises(ValidationError):
+            ContentFeature.objects.create(name="abc", value="x")
+
+    def test_content_feature_create_with_null_name(self):
+        with self.assertRaises(ValidationError):
+            ContentFeature.objects.create(content=self.content, value="x")
+
+    def test_content_feature_create_with_null_value(self):
+        with self.assertRaises(ValidationError):
+            ContentFeature.objects.create(content=self.content, name="abc")
+
+    def test_content_delete_cascade(self):
+        feature = ContentFeature.objects.create(content=self.content, name="abc", value="x")
+
+        self.content.delete()
+        with self.assertRaises(ContentFeature.DoesNotExist):
+            ContentFeature.objects.get(id=feature.id)
