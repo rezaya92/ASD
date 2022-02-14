109bb29145e6775a5043df8f080bb8352efae97f
kmaroufi <kmaroufi76@gmail.com>
2022-02-14 19:35:23 +0330

    Adding file_page and upload_file urls

--- libcloud/urls.py
+++ libcloud/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from ASD import settings
 from . import views
 
 app_name = "homepage"
@@ -7,6 +10,7 @@ urlpatterns = [
     path("", views.homePageView, name="homepage"),
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
-    path("logout", views.logout_request, name= "logout"),
-
+    path("logout", views.logout_request, name="logout"),
+    path("file_page/<str:name>", views.file_page, name="file_page"),
+    path("upload_file", views.upload_file, name="upload_file"),
 ]
