2e835d27aab52c97e3e213d9b227e5157b6826c3
reza <ryarbakhsh@gmail.com>
2022-02-14 17:37:56 +0330

    using django heroku only when in heroku

--- ASD/settings.py
+++ ASD/settings.py
@@ -13,8 +13,6 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import django_heroku
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -125,5 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals(),test_runner = False)
 
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
