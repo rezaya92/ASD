dc1f9cd02da17a7467c9affaadab9c095ecb6495
R Dehghan <rdb1377@gmail.com>
2022-02-14 17:27:06 +0330

    fix

--- ASD/settings.py
+++ ASD/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+django_heroku.settings(locals(),test_runner = False)
 
