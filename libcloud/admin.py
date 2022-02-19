60a9b9f7e1cb902b6f9b768a54108c72b91f9de3
kmaroufi <kmaroufi76@gmail.com>
2022-02-19 21:36:27 +0330

    register ContentType, ContentTypeFeature and AttachmentType models in admin.py

--- libcloud/admin.py
+++ libcloud/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from libcloud.models import Content, ContentFeature, Attachment
+from libcloud.models import Content, ContentFeature, Attachment, AttachmentType, ContentTypeFeature, ContentType
 
 admin.site.register(Content)
 admin.site.register(ContentFeature)
 admin.site.register(Attachment)
+admin.site.register(AttachmentType)
+admin.site.register(ContentType)
+admin.site.register(ContentTypeFeature)
