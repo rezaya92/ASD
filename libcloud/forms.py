7fd2a68134c94a6f72cd331028e4c3e2baad255e
kmaroufi <kmaroufi76@gmail.com>
2022-02-19 21:29:53 +0330

    creating ContentTypeForm and ContentTypeFeatureForm

--- libcloud/forms.py
+++ libcloud/forms.py
@@ -1,13 +1,13 @@
-import mimetypes
-
+from crispy_forms.helper import FormHelper
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your forms here.
-from django.forms import ModelForm, Form
+from django.forms import ModelForm, Form, formset_factory, inlineformset_factory, modelformset_factory, BaseModelFormSet
 
-from libcloud.models import Content, ContentFeature, Attachment
+from libcloud.models import ContentType, ContentTypeFeature, AttachmentType
 
 
 class NewUserForm(UserCreationForm):
@@ -23,3 +23,49 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class ContentTypeForm(ModelForm):
+    class Meta:
+        model = ContentType
+        fields = ("type", "attachment_types")
+        labels = {
+            'type': _('Content type name'),
+        }
+        widgets = {
+            'attachment_types': forms.CheckboxSelectMultiple,
+        }
+
+    prefix = 'content-type'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.user = kwargs["initial"]['user']
+        self.fields['attachment_types'].queryset = AttachmentType.objects.filter(user=self.user)
+
+    def save(self, commit=True):
+        content_type = super().save(commit=False)
+        content_type.user = self.user
+        if commit:
+            content_type.save()
+        return content_type
+
+
+class ContentTypeFeatureForm(ModelForm):
+    class Meta:
+        model = ContentTypeFeature
+        fields = ("name", "type", "required")
+
+    prefix = 'feature'
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline justify-content-center'
+        self.helper.field_template = 'libcloud/layout/inline_field.html'
+        self.helper.form_tag = False
+        super().__init__(*args, **kwargs)
+
+
+ContentTypeFeatureFormset = modelformset_factory(ContentTypeFeature, form=ContentTypeFeatureForm, extra=1,
+                                                 absolute_max=20, max_num=20)
